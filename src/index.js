e2652d5d1fcb7704a424dad99b967013244b1e82
foxten <tenaysiafox@gmail.com>
2020-08-31 17:01:28 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Home from './home'
+import About from './about'
+import Login from './login'
+import SignUp from './signup'
+import Messages from './messages'
+import Navbar from './navbar'
+import {BrowserRouter, Route, NavLink} from 'react-router-dom';
 
-const Home = () => {
-  return (
-    <div>
-      <h1>Home!</h1>
-    </div>
-  );
-};
 
 ReactDOM.render(
-  <Home />,
+  (<BrowserRouter >
+    <div>
+      <Navbar />
+      <Route exact path='/' component={Home} />
+      <Route path='/about' component={About} />
+      <Route path='/login' component={Login} />
+      <Route path='/signup' component={SignUp} />
+      <Route path='/messages' component={Messages} />
+    </div>
+  </BrowserRouter>),
   document.getElementById('root')
 );
---
+++ src/messages.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const Messages = () =>{
+    return (
+      <div>
+        <h1>Here are some messages</h1>
+      </div>
+    )
+  }
+
+export default Messages
